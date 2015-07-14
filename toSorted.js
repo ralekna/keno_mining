9da1f669151a39656e1aae4533630ef27606b8aa
Rytis Alekna <rytis@samesystem.com>
2015-07-14 16:05:37 +0300

    sorted data

---
+++ toSorted.js
@@ -0,0 +1,24 @@
+#!/usr/bin/env node
+
+var data = '';
+ 
+  process.stdin.resume();
+  process.stdin.setEncoding('utf8');
+ 
+  process.stdin.on('data', function(chunk) {
+    data += chunk;
+  });
+ 
+  process.stdin.on('end', function() {
+    sortJSON(data)
+  });
+
+function sortJSON(data) {
+
+	var obj = JSON.parse(data);
+
+	var result = Object.keys(obj).sort().map(function(item) { return obj[item];});
+
+	console.log(result);
+
+}
